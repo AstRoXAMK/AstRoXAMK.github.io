f2ce93bacb94b139671481895838e2e78f1fc50d
Roope Astrén <eroas001@edu.xamk.fi>
2024-09-23 13:50:44 +0300

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config';
+
 import react from '@astrojs/react';
+
 import tailwind from '@astrojs/tailwind';
 const LIVE_URL = 'https://astroxamk.github.io';
 
@@ -7,5 +9,5 @@ const LIVE_URL = 'https://astroxamk.github.io';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: LIVE_URL,
+  site: BASE_URL,
 });
