46c577a4480b865588267cbeda097f4a783c20b1
Roope Astrén <eroas001@edu.xamk.fi>
2024-09-24 09:42:19 +0300

    fuck this

--- .netlify/v1/functions/ssr/.netlify/build/chunks/Intro-sec_01-neQBq.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./Intro-sec_CbAcpb4v.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
