e152a7b869cc9baae24870762145f11e882f7d28
Roope Astrén <eroas001@edu.xamk.fi>
2024-09-24 08:56:08 +0300

    add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,17 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
+import mdx from '@astrojs/mdx';
 const LIVE_URL = 'https://astroxamk.github.io';
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), mdx()],
   site: LIVE_URL,
+  output: 'server',
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
 });
